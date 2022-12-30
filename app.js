2c94502a9ce62aa5d5fea87dadaebee4825e5f84
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-12-30 14:07:08 +0600

    Used OOP

--- app.js
+++ app.js
@@ -1,35 +1,26 @@
 //Global selections and variables
-const colorDivs = document.querySelectorAll('.color');
-const generateBtn = document.querySelector('.generate');
-const sliders = document.querySelectorAll('input[type="range"]');
-const currentHexes = document.querySelectorAll('.color h2');
-let initialColors;
-
-//Functions
-
-//COLOR GENERATOR
-const generateHex = () => {
-    // const letters = '#0123456789ABCDEF';
-    // let hash = '#';
-    // for(let i=0; i<6; i++) {
-    //     hash += letters[Math.floor(Math.random() * 16)]
-    // }
-    // return hash;
+class ColorPicker {
+    constructor() {
+        this.colorDivs =  document.querySelectorAll('.color');
+        this.generateBtn = document.querySelector('.generate');
+        this.sliders = document.querySelectorAll('input[type="range"]');
+        this.currentHexes = document.querySelectorAll('.color h2');
+        this.initialColors;
+    }
+    generateHex() {
     const hexColor = chroma.random();
     return hexColor;
-}
-let randomHex = generateHex();
-console.log(randomHex)
-
-const randomColors = () => {
-    colorDivs.forEach((div, index) => {
-        console.log(div.children[0])
-        const hexText = div.children[0];
-        const randomColor = generateHex()
+    }
+    randomColors() {
+        this.colorDivs.forEach(div => {
+        const hexText = div.children[0]
+        const randomColor = this.generateHex()
         //ADD THE COLOR TO THE BG
         div.style.backgroundColor = randomColor;
         hexText.innerText = randomColor;
-    })
+        })
+    }
 }
 
-randomColors();
+let randomHex = new ColorPicker();
+console.log(randomHex.randomColors())
